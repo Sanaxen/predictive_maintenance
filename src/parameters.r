831d3a22155acd2015a1ad8ed459d7e219ad54a3
Sanaxen <bccatfalcon@gmail.com>
2024-12-20 07:37:07 +0900

    -

--- src/parameters.r
+++ src/parameters.r
@@ -1,98 +1,98 @@
-csv_encoding = 'sjis'
+csv_encoding = 'utf-8'
 base_name = ""
 
-#平滑化でspline
+#spline with smoothing
 use_spline = FALSE
 
-#入力データの分解能
+#Resolution of input data
 unit_of_time = "sec"
 unit_of_record = (1/585936)*60*60*24
 
-#特徴量平滑化
+#Feature Smoothing
 feature_smooth_window = 0
 
-#一度に送られてくるデータ長
+#Length of data sent at a time
 one_input = 24*60*60
 #585936
 
-#入力データ平滑化
+#Input data smoothing
 smooth_window = 0
 smooth_window_slide = 0
-#平滑化をlowessで行う
+#Smoothing is done LOWESS.
 use_lowess = TRUE
 
-#予測に用いる最大データ長
+#Maximum data length for prediction
 #max_data_len = 864000
 
 
-#訓練期間
+#Training period
 max_train_span = 11*30*24*60*60
-#送られてくるデータの最大保持長さ
+#Maximum length of data to be sent and retained
 max_retained_length = 3*30*24*60*60
 
 ###########################################################
-#       以下は移動平均後の数値(入力データ平滑化後)
+#       The following are the values after moving average (after input data smoothing)
 ###########################################################
 sampling_num <- 30
 
-#入力各変数の特徴量（平均、分散、etc)を特徴量にする場合のlookback数
+#Number of lookbacks for features (mean, variance, etc.) for each input variable
 lookback=24*60*60
 lookback_slide = 24*60*60
 
-#特徴量平滑化
+#Feature Smoothing
 smooth_window2 = 12
 smooth_window_slide2 = 1
 
 
-#予測モデル訓練に使う直前の点数
+#Scores just prior to use for predictive model training
 train_num = -60
-#monotonicity計算に使う直前の点数
+#The number of points immediately before the monotonicity calculation
 monotonicity_num = 60
 
 
-#デフォルトの閾値
+#Default Threshold
 threshold = -1000
 
-#plot用Ymax
+#plot Ymax
 ymax = -10000
 ymin =  10000
 
-#各特徴量毎の閾値、Ymaxのパラメータセット
+#Parameter set of threshold and Ymax for each feature
 feature_param = NULL
 
-#予測する未来の長さ閾値
+#Predicted future length threshold
 max_prediction_length = 30*2
 
 
 
-#予測された現在から測定した異常発生時間
+#Time of anomaly occurrence measured from the predicted present
 failure_time_init = max_prediction_length*unit_of_record
 failure_time = failure_time_init
 
-#出力時の時間単位
+#Time unit at output
 forecast_time_unit = "h"
 
-#異常度モデル
+#anomaly model
 m_mahalanobis <- NULL
 
-#送られてくるデータの全てで過去から現在まで
-#予測に用いる最大データ長までに制限したデータフレーム
+#Past to present in all data sent
+#Data frames limited to the maximum data length used for forecasting
 pre = NULL
 pre_org = NULL
-#最大保持長までに制限したデータフレーム
+#Data frames limited to the maximum retention length
 past = NULL
 
-#予測モデル選択
+#Predictive Model Selection
 use_auto_arima = F
 use_arima = T
 use_ets = F
 use_plophet = F
 
 
-#異常を含んだデータがinputできた場合=TRUE
+#If data containing anomalies can be input = TRUE
 abnormality_detected_data <- TRUE
 
-#追跡していく特徴量
+#Features to be tracked
 tracking_feature <- NULL
 dynamic_threshold = TRUE
 watch_name = "X793.."
